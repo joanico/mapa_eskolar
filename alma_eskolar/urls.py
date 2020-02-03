eb19e1ce7ad5ee072885ccfa2e68120fccb21cbf
joanico <joanicobarros@gmail.com>
2020-02-04 00:07:49 +0900

    create view list view and detail for diplay each ebc school

--- alma_eskolar/urls.py
+++ alma_eskolar/urls.py
@@ -1,8 +1,9 @@
-from .views import EbcMapView
+from .views import EbcmapListView, EbcmapDetailView
 from django.urls import path
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', EbcMapView.as_view()),
+    path('', EbcmapListView.as_view(), name='ebcmap-list'),
+    path('ebcmap/<int:pk>/', EbcmapDetailView.as_view(), name='ebcmap-detail'),
 ]
