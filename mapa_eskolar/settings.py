38e8c0edac06b453c01c51f077f3b31d79fe728f
joanico <joanico@catalpa.io>
2020-10-01 14:15:37 +0900

    uninstall django filter

--- mapa_eskolar/settings.py
+++ mapa_eskolar/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 import django_heroku
 import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -166,4 +167,7 @@ OPENSTREETMAP_URL = '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
-
+# This is new:
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
